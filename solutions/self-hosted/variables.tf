3cf8f6d23920d9ac19678ca2b60365e2245b7717
Anilkumar Mallakkanavar <amallak1@in.ibm.com>
2026-07-21 23:39:02 +0530

    addressed review comments

--- solutions/self-hosted/variables.tf
+++ solutions/self-hosted/variables.tf
@@ -59,14 +59,18 @@ variable "tfe_license" {
   sensitive   = true
 }
 
+# renovate: datasource=github-releases depName=hashicorp/terraform-enterprise
 variable "tfe_image_tag" {
   type        = string
-  description = "The version tag of the Terraform Enterprise image to use (e.g., 'v202504-1'). See https://developer.hashicorp.com/terraform/enterprise/releases for available versions."
+  description = "The version tag of the Terraform Enterprise image to use (e.g., 'v2.0.6'). See https://developer.hashicorp.com/terraform/enterprise/releases for available versions."
+  default     = "v2.0.6"
 }
 
+# renovate: datasource=helm depName=terraform-enterprise registryUrl=https://helm.releases.hashicorp.com
 variable "tfe_helm_chart_version" {
   type        = string
-  description = "The version of the Terraform Enterprise Helm chart to use (e.g., '1.6.3'). See https://github.com/hashicorp/terraform-enterprise-helm/blob/main/CHANGELOG.md for available versions."
+  description = "The version of the Terraform Enterprise Helm chart to use (e.g., '1.6.9'). See https://github.com/hashicorp/terraform-enterprise-helm/blob/main/CHANGELOG.md for available versions."
+  default     = "1.6.9"
 }
 
 variable "tfe_helm_repository" {
